1e4a10a91513c7b729804e445128df812db43169
judarura <julianruiz0107@gmail.com>
2025-10-22 12:48:47 -0500

    primer commit con todos los archivos

---
+++ app/src/main/java/com/techhub/SplashTechHub.kt
@@ -0,0 +1,24 @@
+package com.techhub
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashTechHub : AppCompatActivity() {
+
+    private val SPLASH_DURATION = 2000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_tech_zone)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // La lógica de redireccionamiento
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DURATION)
+    }
+}
