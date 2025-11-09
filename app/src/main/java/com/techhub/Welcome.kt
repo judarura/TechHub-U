80fd901d5f389edadc4417ce3dbeff1fbd3142b0
judarura <julianruiz0107@gmail.com>
2025-11-09 16:46:03 -0500

    añadimos el login ffuncional con botoón conectado al incio, redirigiendolo al formulario,

--- app/src/main/java/com/techhub/Welcome.kt
+++ app/src/main/java/com/techhub/Welcome.kt
@@ -1,20 +1,25 @@
 package com.techhub
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
 class Welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_welcome)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnLogin = findViewById<Button>(R.id.btnInicioSesion)
+
+        btnLogin.setOnClickListener {
+            // Crear el Intent para abrir LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
     }
+
 }
